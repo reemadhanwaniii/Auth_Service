7fe526ba7c4b86fcd3f5372ba3bb262acdf13050
Reema Dhanwani <reemadhanwani1234@gmail.com>
2023-03-17 18:40:13 +0530

    Added userById in repo

--- src/index.js
+++ src/index.js
@@ -5,13 +5,18 @@ const apiRoutes = require('./routes/index');
 
 const { PORT } = require('./config/serverConfig.js');
 
+//const Repo = require('./repository/user-repository');
+
 const prepareAndStartServer = () => {
 
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended:true}));
     app.use('/api',apiRoutes);
-    app.listen(PORT,() => {
+    app.listen(PORT,async () => {
         console.log(`Server Started on Port : ${PORT}`);
+        // const repo = new Repo();
+        // const response =await  repo.getById(1);
+        // console.log(response);
     });
 }
 
