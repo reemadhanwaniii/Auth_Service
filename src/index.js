819c841c6ae7e95a8772d12561a80807021dedde
Reema Dhanwani <reemadhanwani1234@gmail.com>
2023-03-17 19:05:12 +0530

    Added jwt create and verify logic

--- src/index.js
+++ src/index.js
@@ -4,18 +4,20 @@ const bodyParser = require('body-parser');
 const apiRoutes = require('./routes/index');
 
 const { PORT } = require('./config/serverConfig.js');
+//const  UserService  = require('./services/user-service');
 
-//const Repo = require('./repository/user-repository');
-
-const prepareAndStartServer = () => {
+const prepareAndStartServer = async () => {
 
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended:true}));
     app.use('/api',apiRoutes);
     app.listen(PORT,async () => {
         console.log(`Server Started on Port : ${PORT}`);
-        // const repo = new Repo();
-        // const response =await  repo.getById(1);
+        const service =  new UserService();
+        // const token = service.createToken({email:'reema@admin.com',id:1});
+        // console.log(token);
+        // const tok = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InJlZW1hQGFkbWluLmNvbSIsImlkIjoxLCJpYXQiOjE2NzkwNTk5MTIsImV4cCI6MTY3OTA2MzUxMn0.BlRr8AxrZ_8NNzKokbKa-BJBXfEOrV4OL5u9ZH6hdto';
+        // const response = service.verifyToken(tok);
         // console.log(response);
     });
 }
--- src/services/user-service.js
+++ src/services/user-service.js
@@ -1,4 +1,7 @@
+const jwt = require('jsonwebtoken');
+const {JWT_KEY} = require('../config/serverConfig');
 const UserRepository = require('../repository/user-repository');
+
 
 class UserService {
     constructor()
@@ -22,6 +25,24 @@ class UserService {
             return response;
         } catch(error) {
             console.log("Something went wrong at the service layer");
+            throw error;
+        }
+    }
+    createToken(user) {
+        try {
+            const result = jwt.sign(user,JWT_KEY,{expiresIn:'1h'});
+            return result;
+        } catch(error) {
+            console.log("Something went wrong in token creation");
+            throw error;
+        }
+    }
+    verifyToken(token) {
+        try {
+            const response = jwt.verify(token,JWT_KEY);
+            return response;
+        } catch(error) {
+            console.log("Something went wrong in token validation",error);
             throw error;
         }
     }
