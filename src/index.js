ef22816516dfc30fa89aa6936952b53350e01a7a
Reema Dhanwani <reemadhanwani1234@gmail.com>
2023-03-17 19:12:46 +0530

    Added function to compare incoming password with encrypted password

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const apiRoutes = require('./routes/index');
 
 const { PORT } = require('./config/serverConfig.js');
-//const  UserService  = require('./services/user-service');
+
 
 const prepareAndStartServer = async () => {
 
@@ -13,12 +13,6 @@ const prepareAndStartServer = async () => {
     app.use('/api',apiRoutes);
     app.listen(PORT,async () => {
         console.log(`Server Started on Port : ${PORT}`);
-        const service =  new UserService();
-        // const token = service.createToken({email:'reema@admin.com',id:1});
-        // console.log(token);
-        // const tok = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InJlZW1hQGFkbWluLmNvbSIsImlkIjoxLCJpYXQiOjE2NzkwNTk5MTIsImV4cCI6MTY3OTA2MzUxMn0.BlRr8AxrZ_8NNzKokbKa-BJBXfEOrV4OL5u9ZH6hdto';
-        // const response = service.verifyToken(tok);
-        // console.log(response);
     });
 }
 
